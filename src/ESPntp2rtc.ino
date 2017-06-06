250c6782ab7275151a09a195e1b5616984ee7757
Alvaro Valdebenito <alvarov@met.no>
2017-06-06 15:35:09 +0200

    try to update RTCs
    do not know if will work, see #1

--- src/ESPntp2rtc.ino
+++ src/ESPntp2rtc.ino
@@ -6,25 +6,17 @@ Based on example code from
   https://github.com/SensorsIot/NTPtimeESP
 */
 
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS1307  ds1307;
+RTC_DS3231  ds3231;
+RTC_PCF8523 pcf8523;
+////RTC_PCF8563 pcf8563;
+
 #include <NTPtimeESP.h>
 #include "config.h"
 NTPtime NTP(NTP_POOL); // see config.h
-
-/*
- * The structure contains following fields:
- * struct strDateTime
-{
-  byte hour;
-  byte minute;
-  byte second;
-  int year;
-  byte month;
-  byte day;
-  byte dayofWeek;
-  boolean valid;
-};
- */
-strDateTime dateTime;
 
 void setup() {
   Serial.begin(115200);
@@ -44,19 +36,26 @@ void setup() {
 void loop() {
 
   // first parameter: Time zone in floating point (for India); second parameter: 1 for European summer time; 2 for US daylight saving time (contributed by viewwer, not tested by me)
-  dateTime = NTP.getNTPtime(1.0, 1);
+  strDateTime ntpNow = NTP.getNTPtime(1.0, 1);
 
-  // check dateTime.valid before using the returned time
-  // Use "setSendInterval" or "setRecvTimeout" if required
-  if(dateTime.valid){
-    NTP.printDateTime(dateTime);
+  // check ntpNow.valid before using the returned time
+  if(ntpNow.valid){
+    // convert NTP date/time strcuture to RTC date/time class
+    DateTime rtcNow (           \
+      (uint16_t) ntpNow.year,   \
+      (uint8_t)  ntpNow.month,  \
+      (uint8_t)  ntpNow.day,    \
+      (uint8_t)  ntpNow.hour,   \
+      (uint8_t)  ntpNow.minute, \
+      (uint8_t)  ntpNow.second);
 
-    byte actualHour = dateTime.hour;
-    byte actualMinute = dateTime.minute;
-    byte actualsecond = dateTime.second;
-    int actualyear = dateTime.year;
-    byte actualMonth = dateTime.month;
-    byte actualday =dateTime.day;
-    byte actualdayofWeek = dateTime.dayofWeek;
+    // set/update RTSs on the I2C bus
+    ds1307.adjust(rtcNow);
+    ds3231.adjust(rtcNow);
+    pcf8523.adjust(rtcNow);
+
+    NTP.printDateTime(ntpNow);
+    Serial.println("RTC updated");
   }
+  delay(10000); // 10 secs
 }
